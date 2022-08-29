8681cdd741812e6f7092fddbb8c445b96f45b844
bappykti <bappykti@gmail.com>
2022-08-30 00:00:47 +0600

    country api is done

--- My_API/countries.js
+++ My_API/countries.js
@@ -1,4 +1,4 @@
-console.log("hello countries");
+// console.log("hello countries");
 
 const loadCountries = ()=>{
     fetch('https://restcountries.com/v3.1/all')
@@ -6,19 +6,38 @@ const loadCountries = ()=>{
     .then(data => displayCountries(data))
 }
 const displayCountries = countries =>{
-    const countriesContainer = document.getElementById('coutries-container');
+    // const countryContainer = document.getElementById('countries-container');
     // for (const country of countries){
     //     console.group(country);
     // }
     const countryContainer = document.getElementById('countries-container');
    countries.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
         countryDiv.innerHTML = `
-        <h3>Name :- </h3>
+        <h3>Name :- ${country.name.common}</h3>
+        <h5>Capital :- ${country.capital ? country.capital[0] : 'No Capital'}</h5>
+        <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
         `;
         countryContainer.appendChild(countryDiv);
    });
+}
+const loadCountryDetails = (code) => {
+    //https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('get country details', code);
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetails(data[0]))
+}
+const displayCountryDetails = country =>{
+    console.log(country);
+    const countryDetails = document.getElementById('country-details');
+    countryDetails.innerHTML = `
+    <h2> Details :- ${country.name.common}</h2>
+    <img src = "${country.flags.png}">
+    `;
 }
 loadCountries();
