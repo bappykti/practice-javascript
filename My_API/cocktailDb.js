de15d8277888cdfa651ed41ec5818391e12097c1
Kazi Tanvirul Islam <bappykti@gmail.com>
2022-09-02 00:17:07 +0600

    cocktail is ok

--- My_API/cocktailDb.js
+++ My_API/cocktailDb.js
@@ -1,6 +1,6 @@
 // console.log('cocktail zone is ready');
-const loadCocktail = () => {
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+const loadCocktail = (search) => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`;
     fetch(url)
     .then(res => res.json())
     .then(cocktails => displayCocktails(cocktails.drinks))
@@ -8,6 +8,7 @@ const loadCocktail = () => {
 const displayCocktails = (cocktails) => {
         // console.log(cocktails);   
         const cocktailsContainer = document.getElementById('cocktails-container');
+        cocktailsContainer.innerHTML = '';
         cocktails.forEach(cocktail => {
             console.log(cocktail);
             const cocktalDiv = document.createElement('div');
@@ -18,7 +19,7 @@ const displayCocktails = (cocktails) => {
                 <div class="card-body">
                 <h5 class="card-title">Name :- ${cocktail.strDrink}</h5>
                 <p class="card-text">Served Glass :- ${cocktail.strGlass}</p>
-                <p class="card-text">Details :- ${cocktail.strInstructions.slice(0, 30)}</p>
+                <p class="card-text">Details :- ${cocktail.strInstructions.slice(0, 50)}</p>
                 </div>
             </div>
             `;
@@ -27,4 +28,10 @@ const displayCocktails = (cocktails) => {
         });    
   
 }
-loadCocktail();
+const searchCocktail = () =>{
+    const searchInputField = document.getElementById('search-input-field');
+    const searchText = searchInputField.value;
+    loadCocktail(searchText);
+    searchInputField.value = "";
+}
+loadCocktail('z');
