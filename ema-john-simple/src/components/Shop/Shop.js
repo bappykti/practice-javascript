cf1bd8da08f620a29a2f2e42f1f53aea495a393e
Kazi Tanvirul Islam <bappykti@gmail.com>
2022-10-02 20:44:21 +0600

    cart interactions are ready

--- ema-john-simple/src/components/Shop/Shop.js
+++ ema-john-simple/src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -27,9 +28,8 @@ const Shop = () => {
                          ></Product>)
                 }
             </div>
-            <div className="cart-container">
-                <h3>Order Summary</h3>
-                <p>Selected Array :- {cart.length}</p>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
